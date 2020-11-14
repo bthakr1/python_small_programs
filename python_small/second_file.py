b489c259f5d5a50f316f89c28f23b479c6012f60
bthakr1 <b.thakr@mail.analytics.hbs.edu>
2020-11-14 14:02:33 -0600

    added index

--- python_small/second_file.py
+++ python_small/second_file.py
@@ -3,6 +3,7 @@ import numpy as np
 
 df = pd.DataFrame({'Yes':[50,21],'No':[131,30]})
 
-df1 = pd.DataFrame({'BOB':["I Liked it","It was wonderful"],"SUE":["I dont like it","It was pathetic"]})
+df1 = pd.DataFrame({'BOB':["I Liked it","It was wonderful"],"SUE":["I dont like it","It was pathetic"]},
+                    index=['Product A','Product B'])
 
 print(df1)
